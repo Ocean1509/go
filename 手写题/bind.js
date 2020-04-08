a0c5316e293470058a5ef854917ed404cc41e81e
wyp <593704361@qq.com>
2020-04-08 22:13:47 +0800

    4.8

--- 手写题/bind.js
+++ 手写题/bind.js
@@ -8,3 +8,15 @@ Function.prototype.myBind = function(context) {
         return fn.apply(context, args.concat(argument))
     }
 }
+
+var a = 23
+var b = 11
+function test(b) {
+    console.log(this.a)
+}
+
+var obj = {
+    a: 1,
+    b: 13
+}
+console.log(test(22))
